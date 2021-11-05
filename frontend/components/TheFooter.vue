601820320a1097144400ea32deb6aaac52ac295c
jordan webber <webber_jordan_95@hotmail.com>
2021-11-05 19:27:30 -0400

    Added basic header and footer

---
+++ frontend/components/TheFooter.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-footer
+    color="#444"
+    padless
+    app
+    bottom
+    fixed
+  >
+    <v-row
+      justify="center"
+      no-gutters
+    >
+      <v-btn
+        v-for="link in links"
+        :key="link"
+        color="white"
+        text
+        rounded
+        class="my-2"
+      >
+        {{ link }}
+      </v-btn>
+      <v-col
+        class="py-4 black text-center white--text"
+        cols="12"
+      >
+        {{ new Date().getFullYear() }} — <strong>Jordan Webber</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      links: [
+        'Home',
+        'About Us',
+        'Team',
+        'Services',
+        'Blog',
+        'Contact Us',
+      ],
+    }),
+  }
+</script>
---
+++ frontend/components/TheHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-app-bar dark color="#444">
+      <v-toolbar-title
+        @click="$router.push('/')"
+        class="headline text-uppercase"
+        style="cursor: pointer"
+      >
+        Workout Tracker
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+      <v-btn text>Bye</v-btn>
+      <v-btn text>Hello</v-btn>
+      <v-btn text>Hello</v-btn>
+      <v-btn text>Hello</v-btn>
+    </v-app-bar>
+  </div>
+</template>
